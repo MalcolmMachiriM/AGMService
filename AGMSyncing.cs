4ec4afb9ae49c055d437fce8d84060f4d8f3260a
Malcolm <98335864+MalcolmMachiriM@users.noreply.github.com>
2023-05-29 12:45:31 +0200

    Initial commit

---
+++ AGMSyncing.cs
@@ -0,0 +1,427 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGMService
+{
+    public enum LookUp
+    {
+        AGMRegisstration = 1,
+        AdminUpdated = 2,
+        AdminResponse = 3,
+        AGMUpdate = 4
+    }
+    public class AGMSyncing
+    {
+        #region variables
+        private MySqlConnection cnxn;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        protected long mID;
+        protected long mEventID;
+        protected long mRegistrationID;
+        protected string mDateCreated;
+        protected bool misSolved;
+        protected string mQuery;
+        protected string mQueryType;
+        protected string mComment;
+        protected string mActionType;
+        protected int mSyncID;
+
+        protected Database portalDB;
+        protected Database liveDB;
+        protected string mConString;
+        protected string mLiveConString;
+        protected int mPortalID;
+        protected string mMsgflg = "";
+
+        #endregion
+        #region Properties
+        public int PortalID
+        {
+            get { return mPortalID; }
+            set { mPortalID = value; }
+        }
+        public int SyncID
+        {
+            get { return mSyncID; }
+            set { mSyncID = value; }
+        }
+        public long ID
+        {
+            get { return mID; }
+            set { mID = value; }
+        }
+
+        public long EventID
+        {
+            get { return mEventID; }
+            set { mEventID = value; }
+        }
+
+        public long RegistrationID
+        {
+            get { return mRegistrationID; }
+            set { mRegistrationID = value; }
+        }
+
+        public string DateCreated
+        {
+            get { return mDateCreated; }
+            set { mDateCreated = value; }
+        }
+
+        public bool isSolved
+        {
+            get { return misSolved; }
+            set { misSolved = value; }
+        }
+
+        public string Query
+        {
+            get { return mQuery; }
+            set { mQuery = value; }
+        }
+
+        public string QueryType
+        {
+            get { return mQueryType; }
+            set { mQueryType = value; }
+        }
+
+        public string Comment
+        {
+            get { return mComment; }
+            set { mComment = value; }
+        }
+
+        public string ActionType
+        {
+            get { return mActionType; }
+            set { mActionType = value; }
+        }
+
+
+        public string Msgflg
+        {
+            get { return mMsgflg; }
+            set { mMsgflg = value; }
+        }
+
+        public string LiveConString
+        {
+            get { return mLiveConString; }
+        }
+
+        public string ConString
+        {
+            get { return mConString; }
+        }
+
+#endregion
+
+        #region methods
+        private bool OpenConnection()
+        {
+            try
+            {
+                cnxn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        LogScriptor.WriteErrorLog("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        LogScriptor.WriteErrorLog("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                cnxn.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                LogScriptor.WriteErrorLog(ex.Message);
+                return false;
+            }
+        }
+
+
+        private void connectToMySQL()
+        {
+            server = "";
+            database = "PensionsManComarton";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + "; DATABASE=" + database + "; UID" + uid + "PASSWORD=" + password + "; Convert Zero DateTime=True; default command timeout = 120;";
+            cnxn = new MySqlConnection(connectionString);
+
+        }
+
+        public AGMSyncing(string ConName, int liveCon)
+        {
+            mConString = ConName;
+            //mLiveConString = liveCon;
+            portalDB = new DatabaseProviderFactory().Create(ConName);
+            liveDB = new DatabaseProviderFactory().Create("penAdmin");
+            //OpenConnection();
+        }
+
+        protected DataSet ReturnDs(string str)
+        {
+
+            try
+            {
+                DataSet ds = new DataSet();
+                ds = portalDB.ExecuteDataSet(CommandType.Text, str);
+                if (ds != null && ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    return ds;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                LogScriptor.WriteErrorLog("An Error occured: " + ex.Message);
+                return null;
+            }
+
+        }
+        public DataSet getQueryFromAgmPortal(int SyncID)
+        {
+            try
+            {
+                System.Data.Common.DbCommand cmd = portalDB.GetStoredProcCommand("sp_Select_AGMQueries");
+                //System.Data.Common.DbCommand cmd = db.GetStoredProcCommand("Get_Participarting_Employer_Portal");
+                portalDB.AddInParameter(cmd, "@SyncID", DbType.Int32, SyncID);
+                DataSet ds = portalDB.ExecuteDataSet(cmd);
+
+
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+
+                    return ds;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogScriptor.WriteErrorLog(ex.Message);
+                return null;
+            }
+        }
+
+        public virtual bool SavetoAdmin()
+        {
+            //Posting New Queries to The Admin DB
+
+            System.Data.Common.DbCommand cmd = liveDB.GetStoredProcCommand("sp_Sync_AGMQueries");
+
+            GenerateSaveParameters(ref liveDB, ref cmd);
+
+
+            try
+            {
+                DataSet ds = liveDB.ExecuteDataSet(cmd);
+
+
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    mID = Convert.ToInt32(ds.Tables[0].Rows[0][0].ToString());
+
+                }
+
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+                mMsgflg = ex.Message;
+                return false;
+
+            }
+
+        }
+
+        public virtual bool SavePortalUpdate()
+        {
+            //Posting New Queries to The Admin DB
+
+            System.Data.Common.DbCommand cmd = portalDB.GetStoredProcCommand("sp_Sync_AGMQueries");
+
+            GenerateSaveParameters(ref portalDB, ref cmd);
+
+
+            try
+            {
+                DataSet ds = portalDB.ExecuteDataSet(cmd);
+
+
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    mID = Convert.ToInt32(ds.Tables[0].Rows[0][0].ToString());
+
+                }
+
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+                mMsgflg = ex.Message;
+                return false;
+
+            }
+
+        }
+
+        public DataSet getQueryFromAdmin(int SyncID)
+        {
+            try
+            {
+                System.Data.Common.DbCommand cmd = liveDB.GetStoredProcCommand("sp_Select_AGMQueries");
+                //System.Data.Common.DbCommand cmd = db.GetStoredProcCommand("Get_Participarting_Employer_Portal");
+                liveDB.AddInParameter(cmd, "@SyncID", DbType.Int32, SyncID);
+                DataSet ds = liveDB.ExecuteDataSet(cmd);
+
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+
+                    return ds;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                mMsgflg = ex.Message;
+                LogScriptor.WriteErrorLog(mMsgflg);
+                return null;
+            }
+        }
+
+
+        public bool UpdateLiveDB(int QueryID, int StatusID)
+        {
+
+
+            System.Data.Common.DbCommand cmd = liveDB.GetStoredProcCommand("sp_Update_AGMQueries_Status");
+            liveDB.AddInParameter(cmd, "@QueryID", DbType.Int32, QueryID);
+            liveDB.AddInParameter(cmd, "@SyncID", DbType.Int32, StatusID);
+
+
+
+            try
+            {
+                DataSet ds = liveDB.ExecuteDataSet(cmd);
+
+
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+                mMsgflg = ex.Message;
+                return false;
+
+            }
+
+        }
+
+        public bool UpdateFromAdmin(int QueryID, int StatusID)
+        {
+            //System.Data.Common.DbCommand cmd = portalDB.GetStoredProcCommand("sp_Sync_AGMQueries");
+            System.Data.Common.DbCommand cmd = portalDB.GetStoredProcCommand("sp_Update_AGMQueries_Status");
+            portalDB.AddInParameter(cmd, "@QueryID", DbType.Int32, QueryID);
+            portalDB.AddInParameter(cmd, "@SyncID", DbType.Int32, StatusID);
+
+            try
+            {
+                DataSet ds = portalDB.ExecuteDataSet(cmd);
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                mMsgflg = ex.Message;
+                return false;
+
+            }
+
+        }
+        protected internal virtual void LoadDataRecord(DataRow rw)
+        {
+
+
+            mID = ((object)rw["ID"] == DBNull.Value) ? 0 : int.Parse(rw["ID"].ToString());
+            mEventID = ((object)rw["EventID"] == DBNull.Value) ? 0 : int.Parse(rw["EventID"].ToString());
+            mRegistrationID = ((object)rw["RegistrationID"] == DBNull.Value) ? 0 : int.Parse(rw["RegistrationID"].ToString());
+            mDateCreated = ((object)rw["DateCreated"] == DBNull.Value) ? "" : rw["DateCreated"].ToString();
+            misSolved = ((object)rw["isSolved"] == DBNull.Value) ? false : bool.Parse(rw["isSolved"].ToString());
+            mQuery = ((object)rw["Query"] == DBNull.Value) ? "" : rw["Query"].ToString();
+            mQueryType = ((object)rw["QueryType"] == DBNull.Value) ? "" : rw["QueryType"].ToString();
+            mComment = ((object)rw["Comment"] == DBNull.Value) ? "" : rw["Comment"].ToString();
+            mActionType = ((object)rw["ActionType"] == DBNull.Value) ? "" : rw["ActionType"].ToString();
+            mSyncID = (int)(((object)rw["SyncID"] == DBNull.Value) ? "" : rw["SyncID"]);
+
+
+        }
+
+        public virtual void GenerateSaveParameters(ref Database db, ref System.Data.Common.DbCommand cmd)
+        {
+            db.AddInParameter(cmd, "@ID", DbType.Int32, mID);
+            db.AddInParameter(cmd, "@EventID", DbType.Int32, mEventID);
+            db.AddInParameter(cmd, "@RegistrationID", DbType.Int32, mRegistrationID);
+            //db.AddInParameter(cmd, "@DateCreated", DbType.String, mDateCreated);
+            db.AddInParameter(cmd, "@isSolved", DbType.Boolean, misSolved);
+            db.AddInParameter(cmd, "@Query", DbType.String, mQuery);
+            db.AddInParameter(cmd, "@QueryType", DbType.String, mQueryType);
+            db.AddInParameter(cmd, "@Comment", DbType.String, mComment);
+            db.AddInParameter(cmd, "@ActionType", DbType.String, mActionType);
+            db.AddInParameter(cmd, "@SyncID", DbType.Int32, mSyncID);
+            db.AddInParameter(cmd, "@PortalID", DbType.Int32, mPortalID);
+        }
+
+        #endregion
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AGMService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer timer1 = (Timer)null;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            this.timer1 = new Timer();
+            this.timer1.Interval = 1000.0; // 60000=5 minutes 120 minutes = 7200000.0  
+
+            this.timer1.Elapsed += new ElapsedEventHandler(this.timer1_tick);
+            this.timer1.Enabled = true;
+            LogScriptor.WriteErrorLog("Agm App Service has started");
+        }
+
+
+        protected void SendUpdatetoAdmin()
+        {
+            try
+            {
+                //1. Insert new queries to admin
+                //LogScriptor.WriteErrorLog("SendUpdatetoAdmin");
+              
+                AGMSyncing obj = new AGMSyncing("cn", 1);
+                DataSet agmquery = obj.getQueryFromAgmPortal((int)LookUp.AGMRegisstration);//agm query
+                if (agmquery != null)
+                {
+                    foreach (DataRow item in agmquery.Tables[0].Rows)
+                    {
+
+                        int RwID = Convert.ToInt32(item["ID"].ToString());
+                        obj.ID = 0;
+                        obj.PortalID = Convert.ToInt32(item["PortalID"].ToString());
+                        obj.EventID = Convert.ToInt32(item["EventID"].ToString());
+                        obj.RegistrationID = Convert.ToInt32(item["RegistrationID"].ToString());
+                        //obj.DateCreated = item["DateCreated"].ToString();
+                        obj.isSolved = Convert.ToBoolean(item["isSolved"].ToString());
+                        obj.Query = item["Query"].ToString();
+                        obj.QueryType = item["QueryType"].ToString();
+                        obj.Comment = item["Comment"].ToString();
+                        obj.ActionType = item["ActionType"].ToString();
+                        obj.SyncID = 2;
+                        if (obj.SavetoAdmin())
+                        {
+                            //2. Update sync status of what has been posted to admin in the portal system
+
+                            LogScriptor.WriteErrorLog("Update Sync status on portal db to 2 for rec id: " + item["PortalID"].ToString());
+                            if (obj.UpdateFromAdmin(RwID, 2)) //Update Portal SYNC status to 2 for admin update
+                            {
+                                LogScriptor.WriteErrorLog("Portal Rec Sync complete");
+                            }
+                            else
+                            {
+                                LogScriptor.WriteErrorLog("Portal Rec Sync Error:" + obj.Msgflg);
+                            }
+                        }
+                        else
+                        {
+                            LogScriptor.WriteErrorLog("Error reported @ syncing a portal query to the live db: " + obj.Msgflg);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogScriptor.WriteErrorLog("Error reported @ sending update to admin system: " + ex.Message);
+            }
+        }
+
+
+        protected void SyncUpdateToPortal()
+        {
+            try
+            {
+                //1. Updating Portal from admin 
+                
+                AGMSyncing obj = new AGMSyncing("cn", 1);
+                DataSet agmquery = obj.getQueryFromAdmin((int)LookUp.AdminResponse);//admin query
+                if (agmquery != null)
+                {
+                    foreach (DataRow item in agmquery.Tables[0].Rows)
+                    {
+
+                        obj.ID = Convert.ToInt32(item["PortalID"].ToString());
+                        obj.EventID = Convert.ToInt32(item["EventID"].ToString());
+                        obj.RegistrationID = Convert.ToInt32(item["RegistrationID"].ToString());
+                        //obj.DateCreated = item["DateCreated"].ToString();
+                        obj.isSolved = Convert.ToBoolean(item["isSolved"].ToString());
+                        obj.Query = item["Query"].ToString();
+                        obj.QueryType = item["QueryType"].ToString();
+                        obj.Comment = item["Comment"].ToString();
+                        obj.ActionType = item["ActionType"].ToString();
+                        obj.SyncID = 4;
+                        if (obj.SavePortalUpdate())
+                        {
+                            //2. Update sync status of what has been update in the admin db to the portal db
+                            int RwID = Convert.ToInt32(item["PortalID"].ToString());
+                            LogScriptor.WriteErrorLog("Update Sync status on portal db to 4 for rec id: " + item["PortalID"].ToString());
+                            if (obj.UpdateLiveDB(RwID, 4))
+                            {
+                                LogScriptor.WriteErrorLog("Admin Rec Sync complete");
+                            }
+                            else
+                            {
+                                LogScriptor.WriteErrorLog("Admin Rec Sync Error:" + obj.Msgflg);
+                            }
+                        }
+                        else
+                        {
+                            LogScriptor.WriteErrorLog("Error reported @ syncing a Admin query to the Portal db: " + obj.Msgflg);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogScriptor.WriteErrorLog("Error reported @ sending update to admin system: " + ex.Message);
+            }
+        }
+        private void timer1_tick(object Sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                
+                SendUpdatetoAdmin();
+               SyncUpdateToPortal();
+
+            }
+            catch (Exception ex)
+            {
+                LogScriptor.WriteErrorLog("Error reported @ getting new open batch header: " + ex.Message);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            this.timer1.Enabled = false;
+            LogScriptor.WriteErrorLog("Agm  service stopped");
+        }
+
+        private void SrvcAction(object Sender, ElapsedEventArgs e)
+        {
+
+        }
+    }
+}
